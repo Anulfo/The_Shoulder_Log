47658b420a4d4e72a64cb5666e298f2383483dfb
Anulfo <noaharkling@gmail.com>
2017-03-21 23:40:20 -0500

    library button disappear when in library page hehehe

--- src/The_Shoulder_Log/wwwroot/js/site.js
+++ src/The_Shoulder_Log/wwwroot/js/site.js
@@ -1,1 +1,7 @@
 ﻿// Write your Javascript code.
+var CurrentUrl = window.location.href;
+
+if (CurrentUrl == 'http://localhost:50663/Physician/Library') {
+    var libraryButton = document.getElementById('libraryButton');
+    libraryButton.style.visibility = "hidden";
+}
